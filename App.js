d0ad2ca05ec11645ae038d4422d89cef628e095a
KevinsProgramming <158289787+KevinsProgramming@users.noreply.github.com>
2024-10-07 15:36:06 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
-import {View, Text, TextInput} from 'react-native';
+import {Text, View, TextInput, Button, Alert} from 'react-native';
+import Logo from ' ./components/Logo';
 import React, {useState} from 'react';
 
-
 export default function App(){
-  const [fullname, setFullname] = useState("Eoin");
+  const [fullname, setFullname] = useState("Joe ");
+  const [lname, setLname] = usestate("Bloggs ");
+  const [dob, setDob] = useState("22 August 1990 ");
+
+  Function buttonClicked() {
+    Alert.alert("Hello" + fname + lname + dob);
+  }
   return(
     <View> 
-      <Text>Hello, World {fullname}</Text>
-      <TextInput placeholder="enter your name" onChangeText={(value)=>setFullname(value)}></TextInput>
+     <Logo/>
+    <TextInput placeholder="Enter your firstname" onChangeText={(val)} =>
+     setFname(val)}/>
+     <TextInput placeholder="Enter your lastname" onChangeText={(val)} =>
+       setLname(val)}/>
+         <TextInput placeholder="Enter your date of birth" onChangeText={(val)} =>
+         setDob(val)}/>
+           
+      <Text>Hello, {fname} {lname}. You were born on {dob}</Text>
+      <Button title="SUBMIT" onPress={buttonClicked}/>
     </View>
   )
-};
+}
+
 
 
 
